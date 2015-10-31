1a3531ce1516542bd8a2fc30fa7573621e474f12
peryamobee <pery@amobee.com>
2015-10-31 22:12:25 +0200

    improve inspector by a little bit

--- inspector3.js
+++ inspector3.js
@@ -2,10 +2,26 @@
  * Created by pery on 31/10/2015.
  */
 (function () {
+    function copyText(text){
+        var select = document.querySelector('#clipboard');
+        select.value = text;
+        select.select();
+
+        try {
+            document.execCommand('copy');
+            alert('Text copied to clipboard!');
+        } catch (err) {
+            window.prompt("To copy the text to clipboard: Ctrl+C, Enter", code);
+        }
+
+    }
+
     var divInsoector;
 
     crel (document.body
         ,divInsoector = crel('div')
+        ,crel('textarea',{id:'clipboard'})
+
     );
 
 
@@ -17,8 +33,19 @@
         input.title ='# '+ input.id ;
         input.addEventListener('click',function () {
             divInsoector.innerHTML = '';
+            var codeElm
+                , codeText = 'document.getElementById(\''+this.id+'\')';
+
             crel(divInsoector,'id: ', this.id
-                ,crel('div','document.getElementById(\'',this.id,'\')'));
+                ,codeElm = crel('div',
+                    crel('span','code: '),
+                    crel('span',{class:'code',title:'click to copy'},codeText)
+                )
+            );
+            codeElm.addEventListener('click', function () {
+                copyText(codeText)
+            })
+
         })
     });
 
